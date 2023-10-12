f69d76483175417d36d5c9e5ebe4d6cad0ee0c8d
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-12 16:01:50 +0800

    use rainbowkit to connect wallet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './res/logo.png';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,6 +7,25 @@ import { DappContextProvider } from './store/contextProvider';
 import RouterPath from './router/router';
 import { HashRouter as Router } from 'react-router-dom';
 import styled from 'styled-components';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains([mainnet, polygon, optimism, arbitrum, base, zora], [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]);
+const { connectors } = getDefaultWallets({
+    appName: 'one-click',
+    projectId: '9ae1de7d5c0b0cc4123c7b1c4088dbf7',
+    chains,
+});
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+});
 
 const GlobalLayout = styled('div')`
     min-height: 100vh !important;
@@ -25,18 +43,22 @@ const PanelLayout = styled('div')`
 
 function App() {
     return (
-        <DappContextProvider>
-            <GlobalLayout>
-                <Router>
-                    <Header></Header>
-                    <PanelLayout>
-                        <RouterPath></RouterPath>
-                    </PanelLayout>
-                    <Footer></Footer>
-                </Router>
-                <GlobalStyle />
-            </GlobalLayout>
-        </DappContextProvider>
+        <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains}>
+                <DappContextProvider>
+                    <GlobalLayout>
+                        <Router>
+                            <Header></Header>
+                            <PanelLayout>
+                                <RouterPath></RouterPath>
+                            </PanelLayout>
+                            <Footer></Footer>
+                        </Router>
+                        <GlobalStyle />
+                    </GlobalLayout>
+                </DappContextProvider>
+            </RainbowKitProvider>
+        </WagmiConfig>
     );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,11 @@
-import { Button, notification } from 'antd';
-import { useState } from 'react';
-// import { useDappContext } from '../store/contextProvider';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import logo from '../res/oneclick.png';
-import { useDappContext } from '../store/contextProvider';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const HeaderTop = styled.div`
     display: flex;
-  align-items: center;
+    align-items: center;
     justify-content: space-between;
     width: 100vw;
     padding: 20px 5%;
@@ -16,47 +13,31 @@ const HeaderTop = styled.div`
 `;
 
 const Logo = styled.div`
-  padding-top: .1rem;
-  img {
-       width: 80px;
+    padding-top: 0.1rem;
+    img {
+        width: 80px;
     }
 `;
 
 export default function Header() {
-
-    const [account, setAccount] = useState('');
-
-    const { dispatch } = useDappContext();
     const navigate = useNavigate();
 
-    const connectWallet = async () => {
-        if ('undefined' !== typeof window.ethereum) {
-            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            console.log(accounts);
-            setAccount(accounts[0]);
-            dispatch({ type: 'set_account', payload: accounts[0] });
-        } else {
-            notification.open({
-                message: 'WTF?',
-                description:
-                    'Metamask is not installed. Please go to hell and download metamask before you come back.',
-                onClick: () => {
-                    console.log('Metamask not installed!');
-                },
-            });
-        }
+    const backToHome = async () => {
+        navigate('/home');
     };
 
-    const backToHome = async() => {
-        navigate('/home');
-    }
-
-    return <HeaderTop>
-        <Logo>
-            <img src={logo} alt="" onClick={()=>{backToHome()}}/>
-        </Logo>
-
-        {!account.length && <Button type='primary' onClick={() => connectWallet()}>Connect Metamask</Button>}
-        {!!account.length && <div>{account}</div>}
-    </HeaderTop>
+    return (
+        <HeaderTop>
+            <Logo>
+                <img
+                    src={logo}
+                    alt=""
+                    onClick={() => {
+                        backToHome();
+                    }}
+                />
+            </Logo>
+            <ConnectButton showBalance accountStatus="address" />
+        </HeaderTop>
+    );
 }
