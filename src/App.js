1511e23303d0efd6d6a164cc916cf623acc0fccf
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-12 15:37:12 +0800

    format code

--- src/App.js
+++ src/App.js
@@ -10,34 +10,34 @@ import { HashRouter as Router } from 'react-router-dom';
 import styled from 'styled-components';
 
 const GlobalLayout = styled('div')`
-min-height: 100vh!important;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-`
+    min-height: 100vh !important;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
 
 const PanelLayout = styled('div')`
-flex-grow: 1;
-display:flex;
-flex-direction:column;
-justify-content:flex-start;
-`
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+`;
 
 function App() {
-  return (
-    <DappContextProvider>
-      <GlobalLayout>
-        <Router>
-          <Header></Header>
-          <PanelLayout>
-            <RouterPath></RouterPath>
-          </PanelLayout>
-          <Footer></Footer>
-        </Router>
-        <GlobalStyle />
-      </GlobalLayout>
-    </DappContextProvider>
-  );
+    return (
+        <DappContextProvider>
+            <GlobalLayout>
+                <Router>
+                    <Header></Header>
+                    <PanelLayout>
+                        <RouterPath></RouterPath>
+                    </PanelLayout>
+                    <Footer></Footer>
+                </Router>
+                <GlobalStyle />
+            </GlobalLayout>
+        </DappContextProvider>
+    );
 }
 
 export default App;
